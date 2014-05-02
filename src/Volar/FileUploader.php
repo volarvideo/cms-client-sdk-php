8acf798216893c0efe5f9f4d1e21cad063177106
gabe rankin <gabe.rankin@gmail.com>
2014-05-02 12:09:27 -0400

    *might* have s3 upload working.

--- src/Volar/FileUploader.php
+++ src/Volar/FileUploader.php
@@ -21,14 +21,52 @@ class FileUploader {
 		);
 		try
 		{
-			$aws = \Aws\Common\Aws::factory(array());
-			$s3 = $aws->get('s3');
-			$req = $s3->put($res['signed_url'], null, fopen($file_path, 'r'));
-			$response = $req->send();
-			if($response->isError())
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $res['signed_url']);
+			curl_setopt($ch, CURLOPT_PUT, 1);
+			$fh = fopen($file_path, 'r');
+			curl_setopt($ch, CURLOPT_INFILE, $fh);
+			curl_setopt($ch, CURLOPT_INFILESIZE, filesize($file_path));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			if(!$res = curl_exec($ch))
 			{
-				throw new Exception("Error when attempting to upload $file_path: \n".$response->json());
+				curl_close($ch);
+				fclose($fh);
+				throw new Exception("cURL error: ($url) ".$error);
 			}
+			curl_close($ch);
+			fclose($fh);
+			if(class_exists('DOMDocument'))
+			{
+				$err_string = '';
+				$doc = new DOMDocument('1.0');
+				$doc->loadXML($xml);
+				$errors = $doc->getElementsByTagName('Error');
+				if(count($errors) > 0)
+				{
+					foreach($errors as $err)
+					{
+						$code = $err->getElementsByTagName('Code');
+						$message = $err->getElementsByTagName('Message');
+						if($code->length)
+							$error_string .= $code->item(0)->textContent.': ';
+						if($message->length)
+							$error_string .= $message->item(0)->textContent;
+					}
+				}
+				if($err_string)
+					throw new Exception($err_string);
+			}
+			echo htmlspecialchars($res);
+
+			// $aws = \Aws\Common\Aws::factory(array('key' => 'fake', 'secret' => 'fake'));
+			// $s3 = $aws->get('s3');
+			// $req = $s3->put($res['signed_url'], null, fopen($file_path, 'r'));
+			// $response = $req->send();
+			// if($response->isError())
+			// {
+			// 	throw new Exception("Error when attempting to upload $file_path: \n".$response->json());
+			// }
 		}
 		catch(Exception $e)
 		{
