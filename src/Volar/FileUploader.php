b30f1683da42577127c37dfd1f715b0385dd6df7
gabe rankin <gabe.rankin@gmail.com>
2014-05-02 17:26:19 -0400

    yeah allright that's cool now you can upload straight to s3

--- src/Volar/FileUploader.php
+++ src/Volar/FileUploader.php
@@ -15,58 +15,30 @@ class FileUploader {
 		if(!$res)
 			throw new \Exception($this->volarObject->error);
 
-		$returnVals = array(
-			'tmp_file_id' => $res['id'],
-			'tmp_file_name' => $res['path']
-		);
 		try
 		{
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $res['signed_url']);
-			curl_setopt($ch, CURLOPT_PUT, 1);
-			$fh = fopen($file_path, 'r');
-			curl_setopt($ch, CURLOPT_INFILE, $fh);
-			curl_setopt($ch, CURLOPT_INFILESIZE, filesize($file_path));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			if(!$res = curl_exec($ch))
+			$returnVals = array(
+				'tmp_file_id' => $res['id'],
+				'tmp_file_name' => $res['key']
+			);
+			$factory_args = array(
+				'key' => $res['access_key'],
+				'secret' => $res['secret'],
+				'token'  => $res['token']
+			);
+			$put_args = array(
+				'ACL' => 'public-read',
+				'Key' => $res['key'],
+				'Bucket' => $res['bucket'],
+				'SourceFile' => $file_path,
+				'ContentDisposition' => "attachment; filename=\"".str_replace(array('"'), "", basename($file_path))."\"",
+			);
+			$s3 = \Aws\S3\S3Client::factory($factory_args);
+			$put_res = $s3->putObject($put_args);
+			if(!$put_res['ObjectURL'])
 			{
-				curl_close($ch);
-				fclose($fh);
-				throw new \Exception("cURL error: ($url) ".$error);
+				throw new \Exception("Could not upload file");
 			}
-			curl_close($ch);
-			fclose($fh);
-			if(class_exists('DOMDocument'))
-			{
-				$err_string = '';
-				$doc = new DOMDocument('1.0');
-				$doc->loadXML($xml);
-				$errors = $doc->getElementsByTagName('Error');
-				if(count($errors) > 0)
-				{
-					foreach($errors as $err)
-					{
-						$code = $err->getElementsByTagName('Code');
-						$message = $err->getElementsByTagName('Message');
-						if($code->length)
-							$error_string .= $code->item(0)->textContent.': ';
-						if($message->length)
-							$error_string .= $message->item(0)->textContent;
-					}
-				}
-				if($err_string)
-					throw new \Exception($err_string);
-			}
-			echo htmlspecialchars($res);
-
-			// $aws = \Aws\Common\Aws::factory(array('key' => 'fake', 'secret' => 'fake'));
-			// $s3 = $aws->get('s3');
-			// $req = $s3->put($res['signed_url'], null, fopen($file_path, 'r'));
-			// $response = $req->send();
-			// if($response->isError())
-			// {
-			// 	throw new Exception("Error when attempting to upload $file_path: \n".$response->json());
-			// }
 		}
 		catch(\Exception $e)
 		{
